05af4321d53e6a233577f56a4a3db02af124e98d
Will Lehman <william.p.lehman@usace.army.mil>
2020-08-07 21:09:04 -0700

    adding national map topo esri rest tileservice

--- src/app-bundles/draw-bundle.js
+++ src/app-bundles/draw-bundle.js
@@ -1,4 +1,12 @@
 
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import XYZ from 'ol/source/XYZ';
+import {getSource} from '@corpsmap/corpsmap-bundles/utils/layer-utils'
+import {getLayer} from '@corpsmap/corpsmap-bundles/utils/layer-utils'
+
 export default {
     name: 'draw',
     getReducer:() =>{
@@ -22,6 +30,13 @@ export default {
     doDrawInitializeLayer: () => ({dispatch, store}) => {
         dispatch({type: 'DRAW_INITIALIZE_STARTED', payload: {shouldInitalize: false}})
         const map = store.selectMap();
+        const lyr = getLayer({
+            serviceType: 'ArcGIS',
+            url: "https://basemap.nationalmap.gov/ArcGIS/rest/services/USGSTopo/MapServer"
+        })
+        map.addLayer(lyr)
+        //map.removeLayer(map.getLayers()[0])
+        //map.getLayers().insertAt(0, wms);;
         dispatch({
             type: 'DRAW_INITIALIZE_FINISHED'
         })
